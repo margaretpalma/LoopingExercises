eb056254d0f9a7a01e23c40db4120fdada7545d5
margaretpalma <mpalma1496@gmail.com>
2025-10-03 08:47:59 -0400

    while/do while/for loop finished

---
+++ src/main/java/DoWhileLoop.java
@@ -0,0 +1,14 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+    //do while
+
+    int count = 0;
+    do
+    {
+        System.out.println("I love Java!");
+        count += 1;
+    } while(count < 5);
+}
+
+}
